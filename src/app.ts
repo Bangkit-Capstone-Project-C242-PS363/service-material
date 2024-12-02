ed6df069a39f33713e21eeac53fee0110e34b686
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-12-02 18:52:23 +0700

    fix: repo not injected

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express from "express";
 import { createMaterialRoutes } from "./infrastructure/http/routes/material.routes";
 import { MaterialController } from "./infrastructure/http/controllers/material.controller";
 import { MaterialService } from "./domain/services/material.services";
+import { InMemoryMaterialRepository } from "./infrastructure/repository/in-memory-material.repository";
 
 const app = express();
 app.use(express.json());
 
-const materialService = new MaterialService();
+const materialRepository = new InMemoryMaterialRepository();
+const materialService = new MaterialService(materialRepository);
 const materialController = new MaterialController(materialService);
 app.use("/materials", createMaterialRoutes(materialController));
 
