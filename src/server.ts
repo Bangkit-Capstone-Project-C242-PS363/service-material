ff2b79c13edfdaf7546f05472999a96366e16c05
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-12-02 17:39:46 +0700

    init app.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,6 @@
-import express from "express";
+import app from "./app";
+const PORT = process.env.PORT || 3000;
 
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
