b9ecf565ddef3c7ec17b31bb18334d3d5d3cc7ce
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-12-03 01:48:38 +0700

    deploy

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+podman build -t signmaster-material-quiz .
+podman tag signmaster-material-quiz asia-southeast2-docker.pkg.dev/capstone-project-c242-ps363/backend/signmaster-material-quiz
+podman push asia-southeast2-docker.pkg.dev/capstone-project-c242-ps363/backend/signmaster-material-quiz
+gcloud run deploy signmaster-material-quiz --image=asia-southeast2-docker.pkg.dev/capstone-project-c242-ps363/backend/signmaster-material-quiz --platform=managed --region=asia-southeast2 --allow-unauthenticated
