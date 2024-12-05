f8cde247569def8e20845a77f5fd7941d2d13290
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-12-05 16:13:34 +0700

    add test

---
+++ test.sh
@@ -0,0 +1,5 @@
+curl http://34.50.84.107/materials/getchapters | jq 2>/dev/null
+curl http://34.50.84.107/materials/getmaterials/100 | jq 2>/dev/null
+
+curl http://34.50.84.107/quiz/getchapters | jq 2>/dev/null #literally sama dengan materials
+curl http://34.50.84.107/quiz/getquizz/100 | jq 2>/dev/null
