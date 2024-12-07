3df91c6ef63b1f238842ac94c884eec130767d16
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-12-07 15:18:48 +0700

    test: add local or server test

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
-curl http://34.50.84.107/materials/getchapters | jq 2>/dev/null
-curl http://34.50.84.107/materials/getmaterials/100 | jq 2>/dev/null
+HOST=http://34.50.84.107
+HOST=http://localhost:8080
+curl $HOST/materials/getchapters | jq 2>/dev/null
+curl $HOST/materials/getmaterials/100 | jq 2>/dev/null
 
-curl http://34.50.84.107/quiz/getchapters | jq 2>/dev/null #literally sama dengan materials
-curl http://34.50.84.107/quiz/getquizz/100 | jq 2>/dev/null
+curl $HOST/quiz/getchapters | jq 2>/dev/null #literally sama dengan materials
+curl $HOST/quiz/getquizz/100 | jq 2>/dev/null
